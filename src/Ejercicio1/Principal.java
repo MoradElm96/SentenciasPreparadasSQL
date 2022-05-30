e2df91576477cb29e4bfe8039846f989faa3961a
Morad elm <78969860+MoradElm96@users.noreply.github.com>
2022-05-31 00:26:19 +0200

    segundo commit del project

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -4,8 +4,7 @@
  * and open the template in the editor.
  */
 package Ejercicio1;
-    
-import java.awt.BorderLayout;
+
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.File;
@@ -20,7 +19,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-    /*Ejecutar el fichero tienda.sql, que crea la base de datos Tienda, compuesta de las tablas
+/*Ejecutar el fichero tienda.sql, que crea la base de datos Tienda, compuesta de las tablas
  Productos y Ventas.
  Hacer un programa con SENTENCIAS PREPARADAS que actualice la tabla Productos, mediante
  el fichero datos.dat. El fichero tiene la siguiente estructura:
@@ -34,152 +33,144 @@ import java.sql.Statement;
  Se deben controlar los posibles errores.
  Finalmente, sacar un informe en el que aparezca:
  Cod_prod Nombre_Producto Total_Venta(unidades vendidas * precio)*/
-    
-    
 /**
  *
  * @author Morad
  */
 public class Principal {
     //primero corremos el script de la base de datos tienda
-    
+
     public static void main(String[] args) throws FileNotFoundException {
-        
+
         try {
             File f = new File("datos.dat");
             //para poder leer el fichero binario
             FileInputStream fis = new FileInputStream(f);
             DataInputStream dis = new DataInputStream(fis);
-            
-            
+
             //conexion a la base de datos
             Class.forName("com.mysql.jdbc.Driver");
-            String url= "jdbc:mysql://localhost/Tienda";
-            String user ="root";
-            String password="";
-            
+            String url = "jdbc:mysql://localhost/Tienda";
+            String user = "root";
+            String password = "";
+
             Connection con = DriverManager.getConnection(url, user, password);//importar con sql
-            
+
             String cadena = "select * from productos where CodProducto=?";
             //select en sentencia preparada insertar
             PreparedStatement psSelect = con.prepareStatement(cadena);
-            
-            
-            cadena= "insert into productos values(?,?,?,?,?)";
+
+            cadena = "insert into productos values(?,?,?,?,?)";
             PreparedStatement psInsertar = con.prepareStatement(cadena);
-            
-            
-            cadena= "update productos set PrecioUnitario=PrecioUnitario+PrecioUnitario*?/100 + where CodProducto=?";
+
+            cadena = "update productos set PrecioUnitario=PrecioUnitario+PrecioUnitario*?/100 + where CodProducto=?";
             PreparedStatement psUpdate = con.prepareStatement(cadena);
-            
-            
-            cadena= "delete from productos where CodProducto=?";
+
+            cadena = "delete from productos where CodProducto=?";
             PreparedStatement psDelete = con.prepareStatement(cadena);
-               
-           try{
-               
-               String opcion;
-               while(true){
-                  //aqui se lee el contenido del fichero y dependiendo de lo que tenga hacemos un switch
-                       opcion= dis.readUTF();
-                       switch(opcion){
-                           case "A": DarDeAlta(dis,psSelect,psInsertar);
-                                break;
-                           case "B": DarBaja(dis,psDelete);
-                                break;
-                           case "M" : Modificar(dis, psUpdate);
-                                break;
-                       }
-                   
-               }
-                
-            }catch(EOFException e){}
-           
-           
-                //cerramos
-                dis.close();
-                fis.close();
-                
-              //aqui el informe
-              
-              Statement st = con.createStatement();
-              cadena= "select CodProducto,sum(unidadesvendidas) from ventas group by CodProducto";
-              
-              ResultSet rs= st.executeQuery(cadena);
-              ResultSet rs1;
-              
-              while(rs.next()){
-                  psSelect.setString(1, rs.getString(1));//la columna
-                  rs1= psSelect.executeQuery();
-                  rs1.next();
-                  System.out.println(rs1.getString(1)+" "+rs1.getString(2)+" "+(rs1.getInt(4)*rs.getInt(2)));
-                  
-                          
 
-              }
-              
-              st.close();
-              con.close();
-                
-                
-                
-            } 
-           
-        catch (IOException e) {  }
-        catch (ClassNotFoundException ex) {  }
-        catch (SQLException ex) { }
-        
-        
+            try {
+
+                String opcion;
+                while (true) {
+                    //aqui se lee el contenido del fichero y dependiendo de lo que tenga hacemos un switch
+                    opcion = dis.readUTF();
+                    switch (opcion) {
+                        case "A":
+                            DarDeAlta(dis, psSelect, psInsertar);
+                            break;
+                        case "B":
+                            DarBaja(dis, psDelete);
+                            break;
+                        case "M":
+                            Modificar(dis, psUpdate);
+                            break;
+                    }
+
+                }
+
+            } catch (EOFException e) {
+            }
+
+            //cerramos
+            dis.close();
+            fis.close();
+
+            //aqui el informe
+            Statement st = con.createStatement();
+            cadena = "select CodProducto,sum(unidadesvendidas) from ventas group by CodProducto";
+
+            ResultSet rs = st.executeQuery(cadena);
+            ResultSet rs1;
+
+            while (rs.next()) {
+                psSelect.setString(1, rs.getString(1));//la columna
+                rs1 = psSelect.executeQuery();
+                rs1.next();
+                System.out.println(rs1.getString(1) + " " + rs1.getString(2) + " " + (rs1.getInt(4) * rs.getInt(2)));
+
+            }
+
+            st.close();
+            con.close();
+
+        } catch (IOException e) {
+        } catch (ClassNotFoundException ex) {
+        } catch (SQLException ex) {
+        }
+
     }
 
-     static void DarDeAlta(DataInputStream dis, PreparedStatement psSelect, PreparedStatement psInsertar) throws IOException, SQLException {
-         
-       String CodProducto = dis.readUTF();
-       String Nombre = dis.readUTF();
-       String LineaProducto = dis.readUTF();
-       int PrecioUnitario = dis.readInt();
-       int Stock = dis.readInt();
-       
-       //el numero es la poscion de la interrogacion
-       psSelect.setString(1, CodProducto);//para cada interrogacion una sentencia con el set del tipo de dato adecuado
-       ResultSet rs = psSelect.executeQuery();
-       
-       if(rs.next()){
-           System.out.println("NO SE PUEDE DAR DE ALTA PUES YA EXISTE");
-       } else { 
-           psInsertar.setString(1, CodProducto);
-           psInsertar.setString(2, Nombre);
-           psInsertar.setString(3,LineaProducto);
-           psInsertar.setInt(4, PrecioUnitario);
-           psInsertar.setInt(5, Stock);
-           
-           System.out.println("alta realizada");
-       }   
-    }
-     
-     
+    static void DarDeAlta(DataInputStream dis, PreparedStatement psSelect, PreparedStatement psInsertar) throws IOException, SQLException {
 
-     static void DarBaja(DataInputStream dis, PreparedStatement psDelete) throws IOException, SQLException {
-         String CodProducto = dis.readUTF();
-         psDelete.setString(1, CodProducto);
-         
-         if(psDelete.executeUpdate()==0){System.out.println("NO SE HA DADO DE BAJA PUES NO EXISTE EL PRODUCTO");}
-         else{ System.out.println("baja realizada");}
-         
+        String CodProducto = dis.readUTF();
+        String Nombre = dis.readUTF();
+        String LineaProducto = dis.readUTF();
+        int PrecioUnitario = dis.readInt();
+        int Stock = dis.readInt();
+
+        //el numero es la poscion de la interrogacion
+        psSelect.setString(1, CodProducto);//para cada interrogacion una sentencia con el set del tipo de dato adecuado
+        ResultSet rs = psSelect.executeQuery();
+
+        if (rs.next()) {
+            System.out.println("NO SE PUEDE DAR DE ALTA PUES YA EXISTE");
+        } else {
+            psInsertar.setString(1, CodProducto);
+            psInsertar.setString(2, Nombre);
+            psInsertar.setString(3, LineaProducto);
+            psInsertar.setInt(4, PrecioUnitario);
+            psInsertar.setInt(5, Stock);
+
+            System.out.println("alta realizada");
+        }
     }
 
-     static void Modificar(DataInputStream dis, PreparedStatement psUpdate) throws IOException, SQLException {
-         
-         String CodProducto = dis.readUTF();
-         int porcentaje = dis.readInt();
-         
-         psUpdate.setInt(1, porcentaje);
-         psUpdate.setString(2, CodProducto);
-         if(psUpdate.executeUpdate()==0){
-             System.out.println("NO SE HA ACTUALIZADO PUES NO EXISTE EL PRODUCTO");
-         }else{System.out.println("modificacion realizada");}
-         
-         
+    static void DarBaja(DataInputStream dis, PreparedStatement psDelete) throws IOException, SQLException {
+        String CodProducto = dis.readUTF();
+        psDelete.setString(1, CodProducto);
+
+        if (psDelete.executeUpdate() == 0) {
+            System.out.println("NO SE HA DADO DE BAJA PUES NO EXISTE EL PRODUCTO");
+        } else {
+            System.out.println("baja realizada");
+        }
+
+    }
+
+    static void Modificar(DataInputStream dis, PreparedStatement psUpdate) throws IOException, SQLException {
+
+        String CodProducto = dis.readUTF();
+        int porcentaje = dis.readInt();
+
+        psUpdate.setInt(1, porcentaje);
+        psUpdate.setString(2, CodProducto);
+        if (psUpdate.executeUpdate() == 0) {
+            System.out.println("NO SE HA ACTUALIZADO PUES NO EXISTE EL PRODUCTO");
+        } else {
+            System.out.println("modificacion realizada");
+        }
+
     }
 
 }
