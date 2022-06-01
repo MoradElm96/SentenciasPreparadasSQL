7012bed6d868a5b1ca62176fa2bc4af1a7592ad4
Morad elm <78969860+MoradElm96@users.noreply.github.com>
2022-06-01 17:59:20 +0200

    cuarto commit del project

--- src/Ejercicio1ExamenBConFicheroTxt/Principal.java
+++ src/Ejercicio1ExamenBConFicheroTxt/Principal.java
@@ -8,32 +8,114 @@ package Ejercicio1ExamenBConFicheroTxt;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author Alumno
  */
 public class Principal {
+
     public static void main(String[] args) {
-        try {
-        File f = new File("suministros.txt");
-        
-        FileReader fr = new FileReader(f);
-        BufferedReader br = new BufferedReader(fr);
-        
-        String suministro = br.readLine();
-        
-        while(suministro!=null){
-        
-            String [] valores = suministro.split(" ");
-            suministro= br.readLine();
-            
-            System.out.println(suministro);
-                    }
-            br.close();
-            fr.close();
-            
-        }catch(Exception e){}
-        
+       try{
+          File f=new File("suministros.txt");
+          FileReader fr=new FileReader(f);
+          BufferedReader br=new BufferedReader(fr);
+          Class.forName("com.mysql.jdbc.Driver");
+          String url="jdbc:mysql://localhost/Construccion";
+          String user="root";
+          String password="";
+          Connection con=DriverManager.getConnection(url, user, password);
+          String cadena="update Suministra set cantidad=? "
+              + "where numprov=? and numparte=? and numproyecto=?";
+          PreparedStatement ps1=con.prepareStatement(cadena);
+          cadena="delete from Suministra where cantidad=?";
+          PreparedStatement ps2=con.prepareStatement(cadena);
+          cadena="insert into Suministra values(?,?,?,?)";
+          PreparedStatement ps3=con.prepareStatement(cadena);
+          cadena="select cantidad from suministra"
+             +" where numprov=? and numparte=? and numproyecto=?";
+          PreparedStatement ps4=con.prepareStatement(cadena);
+          
+          Statement st=con.createStatement();
+          ResultSet rs=st.executeQuery("select * from suministra");
+          System.out.println("--------TABLA SIN ACTUALIZAR -----------");
+          while(rs.next()){
+           System.out.println(rs.getString(1)+" "+rs.getString(2)
+                   +" "+rs.getString(3)+" "+rs.getInt(4));
+          }
+          
+           
+          
+          
+          String suministro=br.readLine();
+          while(suministro!=null){
+            String[] valores=suministro.split(" ");  
+            Actualizar(ps1,ps4,ps3,valores);
+            suministro=br.readLine();   
+          }
+          ps2.setInt(1,0);
+          int bajas=ps2.executeUpdate();
+          System.out.println("Se han eliminado "+bajas+" suministros");
+                
+          br.close();
+          fr.close();
+                  
+          rs=st.executeQuery("select * from suministra");
+          System.out.println("--------TABLA ACTUALIZADA -----------");
+          while(rs.next()){
+           System.out.println(rs.getString(1)+" "+rs.getString(2)
+                   +" "+rs.getString(3)+" "+rs.getInt(4));
+          }
+          st.close();
+          con.close();
+     }catch(Exception ex){
+           System.out.println(ex);
+       }
     }
+  
+  public static void Actualizar(PreparedStatement ps,PreparedStatement ps1,
+                   PreparedStatement ps2,String[] valor) throws SQLException{
+      int cant=0;
+      ps1.setString(1,valor[0]);
+      ps1.setString(2,valor[1]);
+      ps1.setString(3,valor[2]);
+      ResultSet rs=ps1.executeQuery();
+      if(rs.next()){
+        cant=rs.getInt(1);
+        switch(valor[4]){
+          case "s": cant=cant+Integer.parseInt(valor[3]);
+                    break;
+          case "r": cant=cant-Integer.parseInt(valor[3]);
+                    if(cant<0){
+                      cant=0;
+                      System.out.println("Hay unidades pendientes");
+                    }
+                    break;
+        } 
+        
+        ps.setInt(1, cant);
+        ps.setString(2,valor[0]);
+        ps.setString(3,valor[1]);
+        ps.setString(4,valor[2]);
+        ps.executeUpdate();  
+      
+      }else{    //EL REGISTRO NO EXISTE
+        switch(valor[4]){
+          case "s": ps2.setString(1, valor[0]);
+                    ps2.setString(2, valor[1]);
+                    ps2.setString(3, valor[2]);
+                    ps2.setString(4, valor[3]);
+                    ps2.executeUpdate();
+                    break;
+          case "r": System.out.println("No es posible realizar la operacion");
+                    break;
+        }   
+      }
+ }
 }
